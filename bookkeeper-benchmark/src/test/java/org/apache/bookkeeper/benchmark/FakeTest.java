edefe1ee01d9746f202bba02e113819bf7ae134f
Francesco Mancini <francesco140797@gmail.com>
2020-06-03 11:38:25 +0200

    Added fake test

---
+++ bookkeeper-benchmark/src/test/java/org/apache/bookkeeper/benchmark/FakeTest.java
@@ -0,0 +1,23 @@
+package org.apache.bookkeeper.benchmark;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FakeTest {
+
+	@Test
+	public void testAdd() {
+		Assert.assertEquals(0,0);
+	}
+
+	@Test
+	public void testFooOdd() {
+		Assert.assertEquals(0, 0);
+	}
+
+	@Test
+	public void testFooEven() {
+		Assert.assertEquals(0, 0);
+	}
+
+}
