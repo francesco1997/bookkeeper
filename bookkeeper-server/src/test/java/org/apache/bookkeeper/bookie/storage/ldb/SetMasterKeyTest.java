0be30eea642c3af5e038e6ce13311bb44db069ee
Francesco Mancini <francesco140797@gmail.com>
2020-06-24 18:46:45 +0200

    Increasing statement coverage and branch coverage for SetMasterKeyTest

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/SetMasterKeyTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/SetMasterKeyTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mock;
@@ -20,22 +21,39 @@ import java.util.*;
 import static org.mockito.Mockito.when;
 
 @RunWith(Parameterized.class)
-public class SetMasterKeyTest  {
-
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
+public class SetMasterKeyTest {
 
     private Map<byte[], byte[]> ledgerDataMap;
     private LedgerMetadataIndex ledgerMetadataIndex;
     private Long ledgerId;
     private boolean exist;
-    private byte[] masterKey;
+    private byte[] insertingMasterKey;
     private DbLedgerStorageDataFormats.LedgerData ledgerData;
     private byte[] ledgerIdByte;
+    private byte[] previousMasterKey;
 
+    private FakeKeyValueStorageFactory fakeKeyValueStorageFactory;
 
     @Mock
     private KeyValueStorage keyValueStorage;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    public SetMasterKeyTest(TestInput testInput) {
+        this.ledgerId = testInput.getLedgerId();
+        this.exist = testInput.isExist();
+        this.insertingMasterKey = testInput.getMasterKey();
+        this.previousMasterKey = testInput.getMasterKeyPreviousValue();
+        Class<? extends Exception> expectedException = testInput.getExpectedException();
+        if (expectedException != null) {
+            this.expectedException.expect(expectedException);
+        }
+    }
+
 
     private class FakeKeyValueStorageFactory implements KeyValueStorageFactory {
         private KeyValueStorage keyValueStorage;
@@ -74,21 +92,16 @@ public class SetMasterKeyTest  {
         }
     }
 
-    private FakeKeyValueStorageFactory fakeKeyValueStorageFactory;
-
     @Before
     public void configureMock() throws IOException {
         this.fakeKeyValueStorageFactory = new FakeKeyValueStorageFactory(this.keyValueStorage);
         this.ledgerDataMap = new HashMap<>();
 
         if (this.exist) {
-            this.ledgerData = DbLedgerStorageDataFormats.LedgerData.newBuilder().setExists(true).setFenced(true).setMasterKey(ByteString.EMPTY).build();
+            this.ledgerData = DbLedgerStorageDataFormats.LedgerData.newBuilder().setExists(true).setFenced(true).setMasterKey(ByteString.copyFrom(this.previousMasterKey)).build();
             ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
             buffer.putLong(this.ledgerId);
             this.ledgerIdByte = buffer.array();
-
-
-
             this.ledgerDataMap.put(ledgerIdByte, ledgerData.toByteArray());
         }
 
@@ -96,12 +109,6 @@ public class SetMasterKeyTest  {
         when(keyValueStorage.iterator()).thenReturn(fakeClosableIterator);
     }
 
-    public SetMasterKeyTest(TestInput testInput) {
-        super();
-        this.ledgerId = testInput.getLedjerId();
-        this.exist = testInput.isExist();
-        this.masterKey = testInput.getMasterKey();
-    }
 
     @Parameterized.Parameters
     public static Collection<TestInput> getTestParameters() {
@@ -110,48 +117,73 @@ public class SetMasterKeyTest  {
         /*
             ledgerId: {>0}, {=0}, {<0}
             exist: {true}, {false}
-            masterKey: {null}, {valid}, {empty}
+            masterKey: {valid}, {empty}
          */
-        inputs.add(new TestInput((long) -1, false, "validMasterKey".getBytes()));
-        inputs.add(new TestInput((long) 0, true, new byte[0]));
-        inputs.add(new TestInput((long) 1, true, "validMasterKey".getBytes()));
+        inputs.add(new TestInput((long) -1, false, "validMasterKey".getBytes(), new byte[0], null));
+        inputs.add(new TestInput((long) 0, true, new byte[0], new byte[0], null));
+        inputs.add(new TestInput((long) 1, true, "validMasterKey".getBytes(), new byte[0], null));
+        inputs.add(new TestInput((long) 1, true, new byte[0], "validMasterKey".getBytes(), null));
+        inputs.add(new TestInput((long) 1, true, "anotherMasterKey".getBytes(), "validMasterKey".getBytes(), IOException.class));
+        inputs.add(new TestInput((long) 1, true, "validMasterKey".getBytes(), "validMasterKey".getBytes(), null));
 
         return inputs;
     }
 
     private static class TestInput {
-        private Long ledjerId;
+        private Long ledgerId;
         private boolean exist;
         private byte[] masterKey;
+        private byte[] masterKeyPreviousValue;
+        private Class<? extends Exception> expectedException;
 
-        public TestInput(Long ledjerId, boolean exist, byte[] masterKey) {
-            this.ledjerId = ledjerId;
+
+        public TestInput(Long ledgerId, boolean exist, byte[] masterKey, byte[] masterKeyPreviousValue, Class<? extends Exception> expectedException) {
+            this.ledgerId = ledgerId;
             this.exist = exist;
             this.masterKey = masterKey;
+            this.masterKeyPreviousValue = masterKeyPreviousValue;
+            this.expectedException = expectedException;
+
         }
 
-        public Long getLedjerId() {
-            return ledjerId;
+        public Long getLedgerId() {
+            return ledgerId;
         }
+
         public boolean isExist() {
             return exist;
         }
+
         public byte[] getMasterKey() {
             return masterKey;
+        }
+
+        public byte[] getMasterKeyPreviousValue() {
+            return masterKeyPreviousValue;
+        }
+
+        public Class<? extends Exception> getExpectedException() {
+            return expectedException;
         }
     }
 
     @Test
-    public void setMasterKeyTest() throws IOException {
-
+    public void setMasterKeyTest() throws Exception {
         this.ledgerMetadataIndex = new LedgerMetadataIndex(new ServerConfiguration(), this.fakeKeyValueStorageFactory, "fakePath", new NullStatsLogger());
+        ledgerMetadataIndex.setMasterKey(this.ledgerId, this.insertingMasterKey);
 
-        ledgerMetadataIndex.setMasterKey(this.ledgerId, this.masterKey);
 
         DbLedgerStorageDataFormats.LedgerData actualLedgerData = this.ledgerMetadataIndex.get(this.ledgerId);
 
         Assert.assertNotNull(actualLedgerData);
 
-        Assert.assertEquals(Arrays.toString(this.masterKey), Arrays.toString(actualLedgerData.getMasterKey().toByteArray()));
+        String newMasterKey = Arrays.toString(this.insertingMasterKey);
+        String oldMasterKey = Arrays.toString(this.previousMasterKey);
+
+        String expectedMasterKey = (this.previousMasterKey.length == 0 || oldMasterKey.equals(newMasterKey)) ? newMasterKey : oldMasterKey;
+
+
+        Assert.assertEquals(expectedMasterKey, Arrays.toString(actualLedgerData.getMasterKey().toByteArray()));
+
     }
 }
